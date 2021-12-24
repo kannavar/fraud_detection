c34948fc0479f366d470d7443645fbde04a92f4d
kannavar <koteswararao.a@gmail.com>
2021-12-24 08:40:13 +0530

    added new functions add and multiply

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,17 @@
 
 print("This is my initial data processing program")
+
+a = 4 
+b = 5 
+
+def add(a,b):
+    c=a+b
+    return c
+
+print("Added new function for addition")
+
+def multiply(a,b):
+    d=a*b
+    return b
+
+print("Added the multiply operaiton")
