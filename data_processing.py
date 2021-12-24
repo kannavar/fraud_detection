001e0d55b85cbbfd3ee439c81c5d9c794bcfc862
kannavar <koteswararao.a@gmail.com>
2021-12-24 08:27:04 +0530

    added function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,13 @@
 
 print("This is my initial data processing program")
+
+def addition(a,b):
+    if ( a == 0 ):
+        a = 5
+    elif ( b == 0 ):
+        b = 3
+    
+    return (a+b)
+
+
+print("Sum of the two variables is a+b : ", addition(2,4))
