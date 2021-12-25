9cb25f85673200d6318ad420506503ef3a4ebdff
kannavar <koteswararao.a@gmail.com>
2021-12-25 20:22:07 +0530

    Add code for ML models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,18 @@
+import pandas as pd 
+import numpy as np 
 
-print("This is my machine learning model code ")
+
+
+def ml_model1():
+    print("This is ML model1 code ")
+
+
+def ml_model2():
+    print("This is ML model2 code ")
+
+def ml_model3():
+    print("This is ML model 3")
+
+
+
+print("Model 2 has better performance compared to all")
